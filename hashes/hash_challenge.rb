2d26606768c29ff4c970ff0cd810b301bc7a2817
Matt Kiefer <mattkiefer@gmail.com>
2021-01-05 14:15:45 -0500

    Working as intended, but my basic algorithm has errors; different letters can add to the same result, so 'none' ends up with 'evil', etc.

--- hashes/hash_challenge.rb
+++ hashes/hash_challenge.rb
@@ -20,27 +20,6 @@ puts char_value_sums
 arr_targets = char_value_sums.values.uniq
 puts arr_targets
 
-#char_value_sums.each_pair do |word, num| 
-
-#for each key, iterate through hash, compare key, put in array if a match, and delete item
-
-#array pseudocode:
-#check: if value matches array_source[0], push it in final_array[0] as part of an array, which means push it to
-=begin
-char_value_sums.each do |key, value|
-  arr_targets.each do |arr_val|
-
-    organized_arr[arr_val] = tempArray
-    puts "Key is #{key}, value is #{value}, and arr_val is #{arr_val}"
-    if arr_val == value 
-      organized_arr[arr_val] << key
-    end
-  end
-end
-
-puts organized_arr
-
-=end
 
 #Go through array of values, look at each key/value pair and if a match, pop into an array, at the end of each array loop 
 #push that array into the final array
@@ -55,4 +34,4 @@ arr_targets.each_with_index do |code, idx|
   organized_arr[idx] = matching_words
 end
 
-puts organized_arr
+p organized_arr
