165d5bda99796bb9e0be1a84a0080d9ec7d2d859
Matt Kiefer <mattkiefer@gmail.com>
2021-01-06 08:31:26 -0500

    Clear to me I need more basics practice, especially if statements

---
+++ more-stuff/regex_prog.rb
@@ -0,0 +1,25 @@
+# regex_code.rb
+
+=begin
+Write a program that checks if the sequence of characters "lab" exists in the following strings. If it does exist, print out the word.
+
+"laboratory"
+"experiment"
+"Pans Labyrinth"
+"elaborate"
+"polar bear"
+=end
+
+words = [
+  "laboratory",
+"experiment",
+"Pans Labyrinth",
+"elaborate",
+"polar bear"
+]
+
+words.each do |word|
+  if word =~ /lab/ 
+    puts word
+  end
+end
